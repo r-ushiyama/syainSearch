b0a72f97821e1ad8b34b6b1257b9772f91e5d3ad
ry-ushiyama <ry-ushiyama@virtualex.co,jp>
2020-04-27 16:49:44 +0900

    Refactoring SyainServlet.java

--- src/syain/SyainServlet.java
+++ src/syain/SyainServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
@@ -46,81 +47,119 @@ public class SyainServlet extends HttpServlet {
 				String syainName = request.getParameter("syainName");
 				String syainDeptName = request.getParameter("syainDeptName");
 
-				//String jsRequest = "syaintable";
-				String sql = "";
+				List<Syain> syainList = selectSyainFromDB(syainId, syainName, syainDeptName, jsRequest);
 
-				if(jsRequest.equals("syaintable")){
-					//一覧表示
-					sql = "select \n" +
-							"* \n" +
-							"from \n" +
-							"TR_SYAIN,TR_DEPT \n" +
-							"where TR_SYAIN.DEPT_ID = TR_DEPT.DEPT_ID \n"+
-							"order by ID \n";
-				}else if(jsRequest.equals("setsyainInfo")){
-					sql = "select TR_DEPT.DEPT_NAME,TR_SYAIN.DEPT_ID,ID,NAME,AGE,SEX,PHOTO_ID,JOIN_DATE,LEAVE_DATE,ZIP,PREFECTURE,ADDRESS \n" +
-							"from TR_SYAIN,TR_DEPT \n" +
-							"where 1=1 \n" +
-							"and ID='"+syainId+"' \n" +
-							"and TR_SYAIN.DEPT_ID = TR_DEPT.DEPT_ID \n";
-				}else if(jsRequest.equals("searchSyain")){
-					sql = "select TR_DEPT.DEPT_NAME,TR_SYAIN.DEPT_ID,ID,NAME,AGE,SEX,PHOTO_ID,JOIN_DATE,LEAVE_DATE,ZIP,PREFECTURE,ADDRESS \n" +
-							"from TR_SYAIN,TR_DEPT \n" +
-							"where 1=1 \n" +
-							"and TR_DEPT.DEPT_NAME LIKE '%"+syainDeptName+"%' \n" +
-							"and TR_SYAIN.ID LIKE '%"+syainId+"%' \n" +
-							"and TR_SYAIN.NAME LIKE '%"+syainName+"%' \n" +
-							"and TR_SYAIN.DEPT_ID = TR_DEPT.DEPT_ID \n";
+				// アクセスした人に応答するためのJSONを用意する
+				PrintWriter pw = response.getWriter();
 
-				}else{
-					System.out.println("リクエストが指定されていません");
-				}
-
-
-				//DBのURL,ID,PASSを取得
-				Map<String, String> conInfo = ConnectDb.loadDB();
-
-
-				try (
-					// データベースへ接続します
-					Connection con = DriverManager.getConnection(conInfo.get("url"), conInfo.get("user"), conInfo.get("pass"));
-					// SQLの命令文を実行するための準備をおこないます
-					Statement stmt = con.createStatement();
-					// SQLの命令文を実行し、その結果をResultSet型のrsに代入します
-					ResultSet rs1 = stmt.executeQuery(sql);
-				) {
-					List<Syain> syainList = new ArrayList<Syain>();
-					// SQL実行結果を保持している変数rsから情報を取得
-					while (rs1.next()) {
-						Syain syain = new Syain();
-						syain.setSyainDeptName(rs1.getString("DEPT_NAME"));
-						syain.setSyainId(rs1.getString("ID"));
-						syain.setSyainName(rs1.getString("NAME"));
-						syain.setSyainDeptId(rs1.getString("DEPT_ID"));
-						syain.setSyainAge(rs1.getInt("AGE"));
-						syain.setSyainSex(rs1.getString("SEX"));
-						syain.setSyainZip(rs1.getString("ZIP"));
-						syain.setSyainPrefecture(rs1.getString("PREFECTURE"));
-						syain.setSyainAddress(rs1.getString("ADDRESS"));
-						syain.setSyainPhotoId(rs1.getString("PHOTO_ID"));
-						syain.setSyainJoinDate(rs1.getString("JOIN_DATE"));
-						syain.setSyainLeaveDate(rs1.getString("LEAVE_DATE"));
-
-						syainList.add(syain);
-					}
-
-					// アクセスした人に応答するためのJSONを用意する
-					PrintWriter pw = response.getWriter();
-
-					// JSONで出力する
-					pw.append(new ObjectMapper().writeValueAsString(syainList));
-
-				} catch (Exception e) {
-					throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細：[%s]", e.getMessage()), e);
-				}
+				// JSONで出力する
+				pw.append(new ObjectMapper().writeValueAsString(syainList));
 
 			}
 
+
+	private List<Syain> selectSyainFromDB(String syainId, String syainName, String syainDeptName, String jsRequest) {
+		// TODO 自動生成されたメソッド・スタブ
+
+
+		//String jsRequest = "syaintable";
+		String sql  = defineSqlByRequest(syainId, syainName, syainDeptName, jsRequest);
+
+
+		//DBのURL,ID,PASSを取得
+		Map<String, String> conInfo = ConnectDb.loadDB();
+
+
+		try (
+			// データベースへ接続します
+			Connection con = DriverManager.getConnection(conInfo.get("url"), conInfo.get("user"), conInfo.get("pass"));
+			// SQLの命令文を実行するための準備をおこないます
+			Statement stmt = con.createStatement();
+			// SQLの命令文を実行し、その結果をResultSet型のrsに代入します
+			ResultSet rs1 = stmt.executeQuery(sql);
+		) {
+			List<Syain> syainList = retrieveSyainList(rs1);
+			return syainList;
+
+
+
+		} catch (Exception e) {
+			throw new RuntimeException(String.format("検索処理の実施中にエラーが発生しました。詳細：[%s]", e.getMessage()), e);
+		}finally{
+			return new ArrayList<Syain>();
+		}
+	}
+
+	private List<Syain> retrieveSyainList(ResultSet rs1) throws SQLException {
+		List<Syain> syainList = new ArrayList<Syain>();
+		// SQL実行結果を保持している変数rsから情報を取得
+		while (rs1.next()) {
+			Syain syain = retrieveSyain(rs1);
+
+			syainList.add(syain);
+		}
+		return syainList;
+	}
+
+	private Syain retrieveSyain(ResultSet rs1) throws SQLException {
+		Syain syain = new Syain();
+		syain.setSyainDeptName(rs1.getString("DEPT_NAME"));
+		syain.setSyainId(rs1.getString("ID"));
+		syain.setSyainName(rs1.getString("NAME"));
+		syain.setSyainDeptId(rs1.getString("DEPT_ID"));
+		syain.setSyainAge(rs1.getInt("AGE"));
+		syain.setSyainSex(rs1.getString("SEX"));
+		syain.setSyainZip(rs1.getString("ZIP"));
+		syain.setSyainPrefecture(rs1.getString("PREFECTURE"));
+		syain.setSyainAddress(rs1.getString("ADDRESS"));
+		syain.setSyainPhotoId(rs1.getString("PHOTO_ID"));
+		syain.setSyainJoinDate(rs1.getString("JOIN_DATE"));
+		syain.setSyainLeaveDate(rs1.getString("LEAVE_DATE"));
+		return syain;
+	}
+
+	private String defineSqlByRequest(String syainId, String syainName, String syainDeptName, String jsRequest) {
+		String sql = "";
+		if(jsRequest.equals("syaintable")){
+			//一覧表示
+			sql = createSelectAllSyainSql();
+		}else if(jsRequest.equals("setsyainInfo")){
+			sql = createSelectSyainByIdSql(syainId);
+		}else if(jsRequest.equals("searchSyain")){
+			sql = someMethod(syainId, syainName, syainDeptName);
+
+		}else{
+			System.out.println("リクエストが指定されていません");
+		}
+		return sql;
+	}
+
+	private String someMethod(String syainId, String syainName, String syainDeptName) {
+		return "select TR_DEPT.DEPT_NAME,TR_SYAIN.DEPT_ID,ID,NAME,AGE,SEX,PHOTO_ID,JOIN_DATE,LEAVE_DATE,ZIP,PREFECTURE,ADDRESS \n" +
+				"from TR_SYAIN,TR_DEPT \n" +
+				"where 1=1 \n" +
+				"and TR_DEPT.DEPT_NAME LIKE '%"+syainDeptName+"%' \n" +
+				"and TR_SYAIN.ID LIKE '%"+syainId+"%' \n" +
+				"and TR_SYAIN.NAME LIKE '%"+syainName+"%' \n" +
+				"and TR_SYAIN.DEPT_ID = TR_DEPT.DEPT_ID \n";
+	}
+
+	private String createSelectSyainByIdSql(String syainId) {
+		return "select TR_DEPT.DEPT_NAME,TR_SYAIN.DEPT_ID,ID,NAME,AGE,SEX,PHOTO_ID,JOIN_DATE,LEAVE_DATE,ZIP,PREFECTURE,ADDRESS \n" +
+				"from TR_SYAIN,TR_DEPT \n" +
+				"where 1=1 \n" +
+				"and ID='"+syainId+"' \n" +
+				"and TR_SYAIN.DEPT_ID = TR_DEPT.DEPT_ID \n";
+	}
+
+	private String createSelectAllSyainSql() {
+		return "select \n" +
+				"* \n" +
+				"from \n" +
+				"TR_SYAIN,TR_DEPT \n" +
+				"where TR_SYAIN.DEPT_ID = TR_DEPT.DEPT_ID \n"+
+				"order by ID \n";
+	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
